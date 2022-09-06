7d9faebae3bb77fc889ea1fd92f7da6256c7703a
hclee <hclee@dataeum.kr>
2022-09-06 18:30:45 +0900

    core > user > domain 수정

--- src/main/java/study/springboot/bookmanager/core/user/domain/User.java
+++ src/main/java/study/springboot/bookmanager/core/user/domain/User.java
@@ -1,6 +1,7 @@
 package study.springboot.bookmanager.core.user.domain;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import study.springboot.bookmanager.core.common.entity.BaseEntity;
@@ -21,4 +22,22 @@ public class User extends BaseEntity {
 
     private String email;
 
+    //===생성 메서드===//
+    /**
+     * User 생성
+     */
+    @Builder(builderClassName = "createUserBuilder", builderMethodName = "createUserBuilder")
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    /**
+     * User 수정
+     */
+    @Builder(builderClassName = "updateUserBuilder", builderMethodName = "updateUserBuilder")
+    public void updateUser(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
 }
