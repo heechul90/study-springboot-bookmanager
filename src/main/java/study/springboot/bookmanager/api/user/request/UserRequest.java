be08792e1a4f24e5e5f6282d8e57603bfef02b1e
hclee <heechul4296@gmail.com>
2022-09-06 17:46:46 +0900

    api > controller, request and response 추가

---
+++ src/main/java/study/springboot/bookmanager/api/user/request/UserRequest.java
@@ -0,0 +1,4 @@
+package study.springboot.bookmanager.api.user.request;
+
+public class UserRequest {
+}
---
+++ src/main/java/study/springboot/bookmanager/api/user/response/UserResponse.java
@@ -0,0 +1,4 @@
+package study.springboot.bookmanager.api.user.response;
+
+public class UserResponse {
+}
