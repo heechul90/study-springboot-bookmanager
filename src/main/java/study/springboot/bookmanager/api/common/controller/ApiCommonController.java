13728fb173e577781e64a7fd4e126f1b8f353115
hclee <hclee@dataeum.kr>
2022-09-08 11:05:24 +0900

    apiCommonController 수정

--- src/main/java/study/springboot/bookmanager/api/common/controller/ApiCommonController.java
+++ src/main/java/study/springboot/bookmanager/api/common/controller/ApiCommonController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import study.springboot.bookmanager.core.common.exception.CommonException;
 import study.springboot.bookmanager.core.common.json.ApiJsonError;
 import study.springboot.bookmanager.core.common.json.ApiJsonResult;
 
@@ -25,5 +26,9 @@ public class ApiCommonController {
         return ApiJsonResult.ERROR(HttpStatus.BAD_REQUEST, HttpStatus.BAD_REQUEST.getReasonPhrase(), errors);
     }
 
+    @ExceptionHandler(CommonException.class)
+    public ApiJsonResult commonException(CommonException e) {
+        return ApiJsonResult.ERROR(e.httpStatus(), e.getMessage(), e.getErrors());
+    }
 
 }
